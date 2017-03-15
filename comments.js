d9177ea8f3b1dc669949fe339eac32238031d892
Patryk <patrykiewicz@gmail.com>
2017-03-15 20:53:08 +0100

    modular reducers

---
+++ comments.js
@@ -0,0 +1,53 @@
+import {ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT} from './actions';
+
+function reducerComments(state = [], action) {
+    switch(action.type) {
+        case ADD_COMMENT:
+            return [{
+                    id: action.id,
+                    text: action.text,
+                    votes: 0
+                }, ...state];
+        case REMOVE_COMMENT:
+            return state.comments.filter(comment => comment.id !== action.id);
+        case EDIT_COMMENT:
+            return state.comments.map((comment, index) => {
+                                        if (comment.id === action.id) {
+                                            return {
+                                                text: action.text,
+                                                id: comment.id,
+                                                votes: comment.votes
+                                            };
+                                        } else {
+                                            return comment;
+                                        }
+                                    });
+        case THUMB_UP_COMMENT:
+            return state.comments.map((comment, index) => {
+                                        if (comment.id === action.id) {
+                                            return {
+                                                text: comment.text,
+                                                id: comment.id,
+                                                votes: comment.votes++
+                                            };
+                                        } else {
+                                            return comment;
+                                        }
+                                    });
+        case THUMB_DOWN_COMMENT:
+            return state.comments.map((comment, index) => {
+                                        if (comment.id === action.id) {
+                                            return {
+                                                text: comment.text,
+                                                id: comment.id,
+                                                votes: comment.votes--
+                                            };
+                                        } else {
+                                            return comment;
+                                        }
+                                    });
+        default: return state;
+    }
+}
+
+export default reducerComments;
--- reducer.js
+++ reducer.js
@@ -1,41 +1,6 @@
-import {ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, THUMB_UP_COMMENT, THUMB_DOWN_COMMENT} from './actions';
+import { combineReducers } from 'redux';
+import comments from './comments';
 
-const initialState = {
-    comments: [],
-    users: []
-};
-
-function reducer(state = initialState, action) {
-    switch(action.type) {
-        case ADD_COMMENT:
-            return Object.assign({}, state, {
-                comments: [{
-                    id: action.id,
-                    text: action.text,
-                    votes: 0
-                }, ...state.comments]
-            })
-        case REMOVE_COMMENT:
-            return Object.assign({}, state, {comments: state.comments.filter(comment => comment.id !== action.id)});
-            break;
-        case EDIT_COMMENT:
-            return Object.assign({}, state,
-                {comments: state.comments.forEach((comment, index) => {
-                                                if (comment.id === action.id) comment.text = action.text
-                                            })});
-            break;
-        case THUMB_UP_COMMENT:
-            return Object.assign({}, state,
-                {comments: state.comments.forEach((comment, index) => {
-                                                if (comment.id === action.id) comment.votes++
-                                            })});
-            break;
-        case THUMB_DOWN_COMMENT:
-            return Object.assign({}, state,
-                {comments: state.comments.forEach((comment, index) => {
-                                                if (comment.id === action.id) comment.votes++
-                                            })});
-            break;
-        default: return state;
-    }
-}
+const app = combineReducers({
+    comments
+});
